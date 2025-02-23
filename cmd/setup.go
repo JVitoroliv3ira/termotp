b3a366e3d622963d8c332783d48663e175a27552
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-23 17:41:01 -0300

    feat: Trata entrada do usuário

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,18 +1,41 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"time"
+
+	"github.com/JVitoroliv3ira/termotp/internal/models"
+	"github.com/JVitoroliv3ira/termotp/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+var setupName string
+var setupSecret string
 
 var setupCmd = &cobra.Command{
 	Use:   "setup",
 	Short: "Adiciona uma nova conta TOTP",
 	Long:  "Cadastra um novo serviço TOTP, armazenando sua chave de forma segura.",
 	Run: func(cmd *cobra.Command, args []string) {
+		utils.HandleError(utils.ValidateServiceName(setupName))
+		utils.HandleError(utils.ValidateServiceSecret(setupSecret))
 
+		password, err := utils.PromptPassword()
+		utils.HandleError(err)
+		utils.HandleError(utils.ValidatePassword(password))
+
+		account := models.Account{
+			Name:      setupName,
+			Secret:    setupSecret,
+			CreatedAt: time.Now(),
+		}
+
+		fmt.Printf("Conta '%s' cadastrada com sucesso com a senha '%s'\n", account.Name, password)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(setupCmd)
-	setupCmd.Flags().String("account", "", "Nome da conta (ex: gitlab)")
-	setupCmd.Flags().String("secret", "", "Chave secreta TOTP")
+	setupCmd.Flags().StringVarP(&setupName, "name", "n", "", "Nome da conta (ex: gitlab)")
+	setupCmd.Flags().StringVarP(&setupSecret, "secret", "s", "", "Chave secreta TOTP")
 }
