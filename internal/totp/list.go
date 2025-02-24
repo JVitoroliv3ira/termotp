c8192661d7cc82e99472c1cd08bf18b82221829a
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-23 21:03:39 -0300

    feat: Deixa a funcionalidade de listar mais agradavel

--- internal/totp/list.go
+++ internal/totp/list.go
@@ -4,29 +4,59 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sort"
 	"time"
 
 	"github.com/JVitoroliv3ira/termotp/internal/models"
+	"github.com/fatih/color"
 )
 
 func ShowTOTPList(storageData models.StorageData) {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 
+	accountNames := make([]string, 0, len(storageData.Accounts))
+	for name := range storageData.Accounts {
+		accountNames = append(accountNames, name)
+	}
+	sort.Strings(accountNames)
+
+	boldText := color.New(color.Bold)
+	greenText := color.New(color.FgGreen)
+	yellowText := color.New(color.FgYellow)
+	redText := color.New(color.FgRed)
+	cyanText := color.New(color.FgCyan)
+
 	for {
 		clearScreen()
-		fmt.Println("Códigos TOTP:")
-
 		now := time.Now()
 		secondsRemaining := 30 - (now.Unix() % 30)
 
-		for _, account := range storageData.Accounts {
+		fmt.Println("\nCódigos TOTP:")
+
+		for _, name := range accountNames {
+			account := storageData.Accounts[name]
 			code, err := GenerateTOTP(account.Secret)
 			if err != nil {
 				fmt.Printf("Erro ao gerar código para %s\n", account.Name)
 				continue
 			}
-			fmt.Printf("[%s] %s (Expira em %d segundos)\n", account.Name, code, secondsRemaining)
+
+			timerColor := greenText
+			if secondsRemaining <= 10 {
+				timerColor = yellowText
+			}
+			if secondsRemaining <= 5 {
+				timerColor = redText
+			}
+
+			timeText := fmt.Sprintf("(Expira em %d %s)", secondsRemaining, pluralize("segundo", "segundos", secondsRemaining))
+
+			fmt.Printf("[%s] %s %s\n",
+				cyanText.Sprint(name),
+				boldText.Sprint(code),
+				timerColor.Sprint(timeText),
+			)
 		}
 
 		select {
@@ -40,4 +70,11 @@ func ShowTOTPList(storageData models.StorageData) {
 
 func clearScreen() {
 	fmt.Print("\033[H\033[2J")
+}
+
+func pluralize(singular, plural string, value int64) string {
+	if value == 1 {
+		return singular
+	}
+	return plural
 }
