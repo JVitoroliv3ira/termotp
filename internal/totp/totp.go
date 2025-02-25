21e7581d1bd89f28e8453065defba0c84203e905
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-24 21:06:04 -0300

    feat: Define o tempo disponivel para o TOTP ao gerar o TOTP

--- internal/totp/totp.go
+++ internal/totp/totp.go
@@ -6,11 +6,13 @@ import (
 	"github.com/pquerna/otp/totp"
 )
 
-func GenerateTOTP(secret string) (string, error) {
+func GenerateTOTP(secret string) (string, int, error) {
 	code, err := totp.GenerateCode(secret, time.Now())
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
 
-	return code, nil
+	remainingTime := int(30 - (time.Now().Unix() % 30))
+
+	return code, remainingTime, nil
 }
