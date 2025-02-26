fd86bf7fe6e2d1fb35165dbc2795a475bb7d23dc
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-25 22:27:44 -0300

    test: Skipa teste no github, pois o ambiente nao suporta o clipboard

--- internal/utils/clipboard_test.go
+++ internal/utils/clipboard_test.go
@@ -2,20 +2,28 @@ package utils
 
 import (
 	"errors"
+	"os"
 	"testing"
 
 	"github.com/atotto/clipboard"
 )
 
 func TestCopyToClipboard_WithValidText(t *testing.T) {
-	text := "Texto de teste"
+	if os.Getenv("GITHUB_ACTIONS") == "true" {
+		t.Skip("Ignorando teste de clipboard no GitHub Actions (ambiente sem suporte)")
+	}
 
+	text := "Texto de teste"
 	if got := CopyToClipboard(text); got != nil {
 		t.Errorf("CopyToClipboard() = %q; want nil", got)
 	}
 }
 
 func TestCopyToClipboard_WhenClipboardUnsupported(t *testing.T) {
+	if os.Getenv("GITHUB_ACTIONS") == "true" {
+		t.Skip("Ignorando teste de clipboard no GitHub Actions (ambiente sem suporte)")
+	}
+
 	clipboard.Unsupported = true
 	defer func() { clipboard.Unsupported = false }()
 
