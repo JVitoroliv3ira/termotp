c49070b30dd47941664bad665c130bfd0f56ea8b
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-26 22:09:40 -0300

    fix: Ajusta descrição do comando code.go

--- cmd/code/code.go
+++ cmd/code/code.go
@@ -4,8 +4,8 @@ import "github.com/spf13/cobra"
 
 var CodeCmd = &cobra.Command{
 	Use:   "code",
-	Short: "Gerencie códigos TOTP de autenticação",
-	Long:  "Permite gerar, listar e copiar códigos TOTP, facilitando o uso de autenticação em duas etapas (2FA).",
+	Short: "Gere, copie e liste códigos TOTP",
+	Long:  "Permite gerar, listar eGere, copie e liste códigos TOTP para autenticação em dois fatores (2FA).",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
