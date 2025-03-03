4cf3a6681dbe72c9b6255d8554441f53135af6be
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-03 20:05:46 -0300

    feat: Separa logica de arquivos em um arquivo

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,18 +1,14 @@
 package storage
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"encoding/json"
 	"errors"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime"
 
 	"github.com/JVitoroliv3ira/termotp/internal/models"
-	"golang.org/x/crypto/argon2"
+	"github.com/JVitoroliv3ira/termotp/internal/security"
 )
 
 func getStoragePath() string {
@@ -42,93 +38,13 @@ func getStoragePath() string {
 
 var storageFile = getStoragePath()
 
-func deriveKey(password string) []byte {
-	salt := []byte("static-salt")
-	return argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
-}
-
-func Encrypt(data []byte, password string) ([]byte, error) {
-	key := deriveKey(password)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	nonce := make([]byte, aesGCM.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-
-	ciphertext := aesGCM.Seal(nonce, nonce, data, nil)
-	return ciphertext, nil
-}
-
-func Decrypt(encryptedData []byte, password string) ([]byte, error) {
-	key := deriveKey(password)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	nonceSize := aesGCM.NonceSize()
-	if len(encryptedData) < nonceSize {
-		return nil, errors.New("dados criptografados inválidos")
-	}
-
-	nonce, ciphertext := encryptedData[:nonceSize], encryptedData[nonceSize:]
-	plainData, err := aesGCM.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return nil, errors.New("senha incorreta ou arquivo corrompido")
-	}
-
-	return plainData, nil
-}
-
-func SaveAccount(account models.Account, password string) error {
-	var storageData models.StorageData
-
-	if _, err := os.Stat(storageFile); err == nil {
-		encryptedData, err := os.ReadFile(storageFile)
-		if err != nil {
-			return err
-		}
-
-		decryptedData, err := Decrypt(encryptedData, password)
-		if err != nil {
-			return err
-		}
-
-		if err := json.Unmarshal(decryptedData, &storageData); err != nil {
-			return err
-		}
-	} else {
-		storageData = models.StorageData{Accounts: make(map[string]models.Account)}
-	}
-
-	if _, exists := storageData.Accounts[account.Name]; exists {
-		return errors.New("uma conta com este nome já existe")
-	}
-
-	storageData.Accounts[account.Name] = account
-
+func SaveEncrypted(storageData *models.StorageData, password string) error {
 	jsonData, err := json.Marshal(storageData)
 	if err != nil {
 		return err
 	}
 
-	encryptedData, err := Encrypt(jsonData, password)
+	encryptedData, err := security.Encrypt(jsonData, password)
 	if err != nil {
 		return err
 	}
@@ -136,25 +52,12 @@ func SaveAccount(account models.Account, password string) error {
 	return os.WriteFile(storageFile, encryptedData, 0644)
 }
 
-func SaveAccounts(storageData models.StorageData, password string) error {
-	jsonData, err := json.Marshal(storageData)
-	if err != nil {
-		return err
-	}
+func LoadEncrypted(password string) (*models.StorageData, error) {
+	storageData := &models.StorageData{}
+	storageData.Init()
 
-	encryptedData, err := Encrypt(jsonData, password)
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile(storageFile, encryptedData, 0644)
-}
-
-func LoadAccounts(password string) (models.StorageData, error) {
-	var storageData models.StorageData
-
-	if _, err := os.Stat(storageFile); err != nil {
-		return storageData, errors.New("nenhuma conta cadastrada")
+	if _, err := os.Stat(storageFile); errors.Is(err, os.ErrNotExist) {
+		return storageData, nil
 	}
 
 	encryptedData, err := os.ReadFile(storageFile)
@@ -162,9 +65,9 @@ func LoadAccounts(password string) (models.StorageData, error) {
 		return storageData, err
 	}
 
-	decryptedData, err := Decrypt(encryptedData, password)
+	decryptedData, err := security.Decrypt(encryptedData, password)
 	if err != nil {
-		return storageData, errors.New("senha incorreta ou arquivo corrompido")
+		return storageData, err
 	}
 
 	if err := json.Unmarshal(decryptedData, &storageData); err != nil {
@@ -172,37 +75,4 @@ func LoadAccounts(password string) (models.StorageData, error) {
 	}
 
 	return storageData, nil
-}
-
-func GetAccount(name, password string) (models.Account, error) {
-	storageData, err := LoadAccounts(password)
-
-	if err != nil {
-		return models.Account{}, err
-	}
-
-	account, exists := storageData.Accounts[name]
-	if !exists {
-		return models.Account{}, errors.New("conta não encontrada")
-	}
-
-	return account, nil
-}
-
-func DeleteAccount(name, password string) error {
-	storageData, err := LoadAccounts(password)
-
-	if err != nil {
-		return err
-	}
-
-	_, exists := storageData.Accounts[name]
-
-	if !exists {
-		return errors.New("conta não encontrada")
-	}
-
-	delete(storageData.Accounts, name)
-
-	return SaveAccounts(storageData, password)
 }
