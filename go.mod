2c5717e103c8ef03c55a1022c65dbbc054211e48
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-04 20:43:13 -0300

    feat: Exibe os dados dos TOTPs em uma tabela

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pquerna/otp v1.4.0 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
--- internal/totp/list.go
+++ internal/totp/list.go
@@ -10,6 +10,7 @@ import (
 	"github.com/JVitoroliv3ira/termotp/internal/models"
 	"github.com/JVitoroliv3ira/termotp/internal/utils"
 	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
 )
 
 func ShowTOTPList(storageData models.StorageData) {
@@ -22,41 +23,69 @@ func ShowTOTPList(storageData models.StorageData) {
 	}
 	sort.Strings(accountNames)
 
-	boldText := color.New(color.Bold)
-	greenText := color.New(color.FgGreen)
-	yellowText := color.New(color.FgYellow)
-	redText := color.New(color.FgRed)
-	cyanText := color.New(color.FgCyan)
+	bold := color.New(color.Bold)
+	green := color.New(color.FgGreen)
+	yellow := color.New(color.FgYellow)
+	red := color.New(color.FgRed)
 
 	for {
 		clearScreen()
+		fmt.Println(bold.Sprint("\n CÓDIGOS TOTP"))
 
-		fmt.Println("\nCódigos TOTP:")
+		if len(accountNames) == 0 {
+			fmt.Println("\nNenhuma conta cadastrada. Adicione uma conta para gerar códigos TOTP.")
+			return
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{" CONTA ", " CÓDIGO ", " EXPIRA EM "})
+		table.SetBorder(true)
+		table.SetCenterSeparator("|")
+		table.SetColumnSeparator("|")
+		table.SetRowSeparator("-")
+		table.SetAlignment(tablewriter.ALIGN_CENTER)
+
+		table.SetColMinWidth(0, 15)
+		table.SetColMinWidth(1, 10)
+		table.SetColMinWidth(2, 15)
+		table.SetAutoWrapText(false)
+		table.SetHeaderAlignment(tablewriter.ALIGN_CENTER)
+		table.SetColumnAlignment([]int{
+			tablewriter.ALIGN_CENTER,
+			tablewriter.ALIGN_CENTER,
+			tablewriter.ALIGN_CENTER,
+		})
+		table.SetHeaderLine(true)
+		table.SetRowLine(true)
+		table.SetAutoFormatHeaders(false)
 
 		for _, name := range accountNames {
 			account := storageData.Accounts[name]
 			code, secondsRemaining, err := GenerateTOTP(account.Secret)
 			if err != nil {
-				fmt.Printf("Erro ao gerar código para %s\n", account.Name)
+				table.Append([]string{name, "Erro ao gerar", "-"})
 				continue
 			}
 
-			timerColor := greenText
-			if secondsRemaining <= 10 {
-				timerColor = yellowText
-			}
+			timeText := fmt.Sprintf("%d %s", secondsRemaining, utils.Pluralize("segundo", "segundos", secondsRemaining))
 			if secondsRemaining <= 5 {
-				timerColor = redText
+				timeText = red.Sprint(timeText)
+			} else if secondsRemaining <= 10 {
+				timeText = yellow.Sprint(timeText)
+			} else {
+				timeText = green.Sprint(timeText)
 			}
 
-			timeText := fmt.Sprintf("(Expira em %d %s)", secondsRemaining, utils.Pluralize("segundo", "segundos", secondsRemaining))
-
-			fmt.Printf("[%s] %s %s\n",
-				cyanText.Sprint(name),
-				boldText.Sprint(code),
-				timerColor.Sprint(timeText),
-			)
+			table.Append([]string{
+				fmt.Sprintf(" %-15s ", name),
+				fmt.Sprintf(" %s ", bold.Sprint(code)),
+				fmt.Sprintf(" %s ", timeText),
+			})
 		}
+
+		table.Render()
+
+		fmt.Println("\nAtualizando... Pressione Ctrl+C para sair.")
 
 		select {
 		case <-stop:
