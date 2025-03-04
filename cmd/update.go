5110b4d67fc4a8896b6c174fb6ef07f8cc52b8b3
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-04 13:55:30 -0300

    feat: Implementa comando responsável por atualizar a versão da aplicação

--- cmd/update.go
+++ cmd/update.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/JVitoroliv3ira/termotp/internal/update"
+	"github.com/JVitoroliv3ira/termotp/internal/utils"
+	"github.com/spf13/cobra"
+)
 
 var updateVersion string
 
@@ -8,7 +14,25 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Atualize o TermOTP",
 	Long:  "Verifique e instale a versão mais recente do TermOTP disponível ou escolha uma versão específica para instalar.",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		u := update.NewUpdater()
+		installed, err := u.Update(updateVersion)
+		utils.HandleError(err)
+
+		if installed {
+			if updateVersion == "" {
+				fmt.Println("TermOTP foi atualizado para a versão mais recente com sucesso!")
+			} else {
+				fmt.Printf("TermOTP foi atualizado para a versão %s com sucesso!\n", updateVersion)
+			}
+		} else {
+			if updateVersion == "" {
+				fmt.Println("TermOTP já está na versão mais recente.")
+			} else {
+				fmt.Printf("A versão %s já estava instalada, portanto nenhuma ação foi realizada.\n", updateVersion)
+			}
+		}
+	},
 }
 
 func init() {
