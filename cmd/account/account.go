b18a74c8642461c96842f16f8de35615ec9285ca
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-07 22:11:58 -0300

    fix: Ajusta Long para conter nova funcionalidade

--- cmd/account/account.go
+++ cmd/account/account.go
@@ -7,7 +7,7 @@ import (
 var AccountCmd = &cobra.Command{
 	Use:   "account",
 	Short: "Gerencie contas vinculadas a TOTP",
-	Long:  "Adicione ou remova contas para geração de códigos TOTP de autenticação em dois fatores.",
+	Long:  "Adicione, remova ou renomeie contas vinculadas ao TOTP para geração de códigos de autenticação em dois fatores.",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
